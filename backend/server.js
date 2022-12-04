71db6f91da3f020cc022150923f9a5d2bd214d06
smalik631 <maliksidra774@gmail.com>
2022-12-04 22:19:42 +0500

    userroutes, token generator

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 // to fetch variables in the env File
 dotenv.config();
@@ -18,8 +19,22 @@ mongoose
   });
 const app = express();
 
+// form data in post request will be converted
+//json object inside rec.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+
+app.use('/api/users', userRouter);
+
+// like a middelware that accept four parameters
+//when erro occur in expressAsynHandle in userroutes the below code run and error
+//msg return to user
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 // define port that we use in response for backend
 
